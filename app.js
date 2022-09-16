2bc0f7dab892dd0269a0532d709b6408fe3a443c
brenley-woodard <brenleyew@gmail.com>
2022-09-16 11:06:19 -0700

    attraction add and displaying attractions working

--- app.js
+++ app.js
@@ -4,15 +4,21 @@
 const planetName = document.getElementById('planet-name');
 const planetLocation = document.getElementById('planet-location');
 const planetArch = document.getElementById('planet-arch');
+
 const nameInput = document.getElementById('planet-name-input');
 const locationSelect = document.getElementById('location-select');
 const archSelect = document.getElementById('arch-select');
+
+const attractionList = document.getElementById('attraction-list');
+const attractionInput = document.getElementById('attraction-input');
+const addAttractionButton = document.getElementById('add-attraction-button');
 
 /* State */
 let planet = {
     name: 'Naboo',
     location: 'underwater',
     arch: 'industrial',
+    attraction: [],
 };
 /* Events */
 nameInput.addEventListener('input', () => {
@@ -33,6 +39,13 @@ archSelect.addEventListener('change', () => {
     displayPlanet();
 });
 
+addAttractionButton.addEventListener('click', () => {
+    const attraction = attractionInput.value;
+    planet.attraction.push(attraction);
+    displayAttraction();
+    attractionInput.value = '';
+});
+
 /* Display Functions */
 function displayPlanet() {
     planetName.textContent = nameInput.value;
@@ -40,6 +53,16 @@ function displayPlanet() {
     planetArch.src = 'arch/' + planet.arch + '.png';
 }
 
+function displayAttraction() {
+    attractionList.innerHTML = '';
+
+    for (let attraction of planet.attraction) {
+        const li = document.createElement('li');
+        li.textContent = attraction;
+        attractionList.append(li);
+    }
+}
 // (don't forget to call any display functions you want to run on page load!)
 
 displayPlanet();
+displayAttraction();
